9b1e96ba4cc07b4927fe1eb953048fc45efe8c4a
satheeshpandianj <52313344+satheeshpandianj@users.noreply.github.com>
2021-08-26 16:17:37 +0530

    Update perfAPITestScript.js

--- scripts/perfAPITestScript.js
+++ scripts/perfAPITestScript.js
@@ -7,7 +7,7 @@ import { sleep } from 'k6';
 import { SharedArray } from 'k6/data';
 const Request = require('../projects/PerfTest/' + __ENV.Project + '/TestScripts/APIRequest.js');
 
-// console.log(`API Names are ${__ENV.APINAME}`);
+console.log(`API Names are ${__ENV.APINAME}`);
 
 export let options = {
     vus: __ENV.USERS,
