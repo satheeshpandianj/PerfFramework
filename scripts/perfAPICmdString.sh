960b3d9b7800d26e84b6c50be7b754e22bffc9d7
satheeshpandianj <52313344+satheeshpandianj@users.noreply.github.com>
2021-08-26 10:27:17 +0530

    Removed Installation

--- scripts/perfAPICmdString.sh
+++ scripts/perfAPICmdString.sh
@@ -11,18 +11,6 @@ DURATION=$6
 IFS=',' read -ra API_FILES <<<"$APINAME"
     echo ${API_FILES[*]}
 #####################################################################################
-##                                K6 installation                                  ##
-#####################################################################################
-sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys C5AD17C747E3415A3642D57D77C6C491D6AC1D69
-echo "deb https://dl.k6.io/deb stable main" | sudo tee /etc/apt/sources.list.d/k6.list
-echo "Updating ubuntu to get upto date.."
-sudo apt-get update
-echo "Installation of K6 starts in Ubuntu.."
-sudo apt-get install k6
-echo "Installation of K6 done in Ubuntu.."      
-echo "Validating of K6 version installed in Ubuntu.."
-k6 version
-#####################################################################################
 ##                                 EXECUTE TESTS                                   ##
 #####################################################################################
 # k6_test='k6 run -e ENV='$ENV' -e Project='$PROJECT' -e APINAME='$APINAME' -e DATA='$DATA' --vus '$USERS' --iterations '$DURATION' ./PerfTest/Projects/'$PROJECT'/perfTestScripts.js'
