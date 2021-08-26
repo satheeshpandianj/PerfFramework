cfb3953d41d6ae8c0c561e58c78a751d6d103688
satheeshpandianj <52313344+satheeshpandianj@users.noreply.github.com>
2021-08-26 14:25:25 +0530

    Added PWD

--- scripts/perfAPICmdString.sh
+++ scripts/perfAPICmdString.sh
@@ -3,8 +3,8 @@ ENV=$1
 PROJECT=$2
 APINAME=$3
 DATA=$4
-#USERS=$5
-#DURATION=$5
+USERS=$5
+DURATION=$6
 #####################################################################################
 ##                                 API FILE SELECTION                              ##
 #####################################################################################
@@ -17,9 +17,10 @@ IFS=',' read -ra API_FILES <<<"$APINAME"
 # k6_test='k6 run -e ENV='$ENV' -e Project='$PROJECT' -e APINAME='$APINAME' -e DATA='$DATA' --vus '$USERS' --iterations '$DURATION' --out influxdb=http://104.40.213.24:8086/'$DBNAME' ./scripts/perfAPITestScript.js'
 # $k6_test
 echo "Performance testing is started.."
+echo $PWD
 #for apiName in "${API_FILES[@]}"; do
-    k6_test='k6 run -e ENV='$ENV' -e Project='$PROJECT' -e APINAME='$apiName' -e DATA='$DATA' /scripts/perfAPITestScript.js'
+    k6_test='k6 run -e ENV='$ENV' -e Project='$PROJECT' -e APINAME='$apiName' -e DATA='$DATA' --vus '$USERS' --duration '$DURATION' /scripts/perfAPITestScript.js'
     $k6_test
-#    sleep 5
+   #sleep 5
 #done
 echo "Performance testing is completed.."
