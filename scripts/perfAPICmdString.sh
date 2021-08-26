a4c27c4f04fe5857b942a66b92836bf6793ccfd3
satheeshpandianj <52313344+satheeshpandianj@users.noreply.github.com>
2021-08-26 16:52:51 +0530

    Update perfAPICmdString.sh

--- scripts/perfAPICmdString.sh
+++ scripts/perfAPICmdString.sh
@@ -5,6 +5,7 @@ APINAME=$3
 DATA=$4
 USERS=$5
 DURATION=$6
+FILENAME='./perfAPITestScript.js'
 #####################################################################################
 ##                                 API FILE SELECTION                              ##
 #####################################################################################
@@ -17,8 +18,8 @@ IFS=',' read -ra API_FILES <<<"$APINAME"
 # k6_test='k6 run -e ENV='$ENV' -e Project='$PROJECT' -e APINAME='$APINAME' -e DATA='$DATA' --vus '$USERS' --iterations '$DURATION' --out influxdb=http://104.40.213.24:8086/'$DBNAME' ./scripts/perfAPITestScript.js'
 # $k6_test
 #for apiName in "${API_FILES[@]}"; do
-k6_test='k6 run -e ENV='$ENV' -e Project='$PROJECT' -e APINAME='$APINAME' -e DATA='$DATA'  -e USERS='$USERS' -e TESTINGTIME='$DURATION' .\perfAPITestScript.js'
+k6_test='-e ENV='$ENV' -e Project='$PROJECT' -e APINAME='$APINAME' -e DATA='$DATA'  -e USERS='$USERS' -e TESTINGTIME='$DURATION''
 echo "$k6_test"
-$k6_test
+k6 run $k6_test $FILENAME
 #sleep 5
 #done
