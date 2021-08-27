925ff47e8bcc5537c2b7272f145b34b274e42626
satheeshpandianj <52313344+satheeshpandianj@users.noreply.github.com>
2021-08-27 12:21:31 +0530

    Included for loop

--- scripts/perfAPICmdString.sh
+++ scripts/perfAPICmdString.sh
@@ -7,5 +7,8 @@ USERS=$5
 DURATION=$6
 IFS=',' read -ra API_FILES <<<"$APINAME"
     echo ${API_FILES[*]}
-k6_test='k6 run -e ENV='$ENV' -e Project='$PROJECT' -e APINAME='$APINAME' -e DATA='$DATA' -e USERS='$USERS' -e TESTINGTIME='$DURATION' ./scripts/perfAPITestScript.js'
-$k6_test
+for apiName in "${API_FILES[@]}";do
+	k6_test='k6 run -e ENV='$ENV' -e Project='$PROJECT' -e APINAME='$apiName' -e DATA='$DATA' -e USERS='$USERS' -e TESTINGTIME='$DURATION' ./scripts/perfAPITestScript.js'
+	$k6_test
+	sleep 5
+done
